14510c7e18960eb05d040f8910cb223efc661527
Humberto Mangino <hmangino@gmail.com>
2020-02-14 10:40:03 -0600

    initial deployment

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { GraphQLServer } = require('graphql-yoga');
+const { Prompts, sequelize} = require('../models');
+
+const typeDefs = `
+	type Prompt {
+		title: String!
+		id: ID!
+	}
+  type Query {
+    prompt: Prompt
+  }
+`
+
+const resolvers = {
+  Query: {
+    prompt: async (_, args) => {
+			const prompt = await Prompts.findOne({
+				order: sequelize.random()
+			});
+			return prompt;
+		}
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(() => console.log(`Server is running at http://localhost:4000`))
