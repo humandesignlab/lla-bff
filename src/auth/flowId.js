f8bb15c4f08145b982b2f70737b818a9dc372c23
Humberto Mangino <hmangino@gmail.com>
2020-02-28 13:58:28 -0600

    OIDC FlowID integration

--- src/auth/flowId.js
+++ src/auth/flowId.js
@@ -7,7 +7,7 @@ module.exports = Issuer.discover(process.env.AUTH0_DOMAIN) // => Promise
     const client = new flowIdIssuer.Client({
       client_id: process.env.AUTH0_CLIENT_ID,
       client_secret: process.env.AUTH0_CLIENT_SECRET,
-      redirect_uri: 'http://freeflow.gotocme.com/completeSignin',
+      redirect_uri: 'http://ec2-3-234-244-10.compute-1.amazonaws.com/',
       response_types: 'code',
 
     }); // => Client
@@ -17,14 +17,11 @@ module.exports = Issuer.discover(process.env.AUTH0_DOMAIN) // => Promise
   
   const code_challenge = generators.codeChallenge(code_verifier);
     client.authorizationUrl({
-      scope: 'openid',
-      resource: 'https://thirdparty.flowid.co/platform/oidc/authorize',
-      code_challenge,
-      code_challenge_method: 'S256'
+      scope: 'openid'
     });
-    const params = client.callbackParams('https://thirdparty.flowid.co/platform/oidc/token');
-    
-    client.callback('http://freeflow.gotocme.com/completeSignin', params, { code_verifier }) // => Promise
+    const params = client.callbackParams('https://thirdparty.flowid.co/platform/oidc/authorize?grant_type=authorization_code&code=Kn4ys9&state=Bwie3MB5GmEJ7KoBNpEuKaWw&');
+    console.log('PARAMS ', params)
+    client.callback('http://ec2-3-234-244-10.compute-1.amazonaws.com/', params, { code_verifier }) // => Promise
       .then(function (tokenSet) {
         console.log('received and validated tokens %j', tokenSet);
         console.log('validated ID Token claims %j', tokenSet.claims());
